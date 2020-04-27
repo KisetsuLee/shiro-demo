38c29a72c07a74e9b8097f0c5ac7737112e62c14
Lee <lizhijie_lee@126.com>
2020-04-27 13:27:27 +0800

    initial

---
+++ src/main/java/com/lee/shiroDemo/ShiroDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lee.shiroDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiroDemoApplication.class, args);
+	}
+
+}
